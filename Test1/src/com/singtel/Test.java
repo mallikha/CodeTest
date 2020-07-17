0464382ea83c6303cb25a7f973773e27f5ff6b58
mallikha <konatham.malli@gmail.com>
2020-07-18 01:41:54 +0800

    git test commit

--- Test1/src/com/singtel/Test.java
+++ Test1/src/com/singtel/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 
 		System.out.println("Testing GIT File123");
-		System.out.println("Testing GIT Fil");
+		System.out.println("test commit for git");
 	}
 
 }
