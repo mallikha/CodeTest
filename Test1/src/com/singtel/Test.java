2d3b4c47c36a08f8a94c055b55c575ee771787df
mallikha <konatham.malli@gmail.com>
2020-07-18 18:11:28 +0800

    code clean up

--- Test1/src/com/singtel/Test.java
+++ Test1/src/com/singtel/Test.java
@@ -4,9 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		System.out.println("Testing GIT File123");
 		System.out.println("test commit for git");
-		System.out.println("test commit for githu123b");
 	}
 
 }
